efaba057418f3c196f405688391ea1a3ee1a5506
toshiaki21 <toshiaki.fujioka21@gmail.com>
2022-06-24 20:26:15 +0900

    一旦コミットします

--- script.js
+++ script.js
@@ -1,44 +1,76 @@
-$(function(){
-    // console.log('test');
+$(function () {
+  // console.log('test');
 
-    $('button').on('click',function(){
+  $('button').on('click', function () {
 
-        $.ajax({
-            url: 'https://yesno.wtf/api', //アクセスするURL
-            type: 'get',    //post or get
-            cache: false,        //cacheを使うか使わないかを設定
-            dataType:'json',     //data type script・xmlDocument・jsonなど
-            // data:parameter,           //アクセスするときに必要なデータを記載      
-          })
-          .done(function(response) { 
-              console.log(response);
-            
-             //通信成功時の処理
-             //成功したとき実行したいスクリプトを記載
-          })
-          .fail(function(xhr) {  
-             //通信失敗時の処理
-             //失敗したときに実行したいスクリプトを記載
-          })
-          .always(function(xhr, msg) { 
-            //通信完了時の処理
-            //結果に関わらず実行したいスクリプトを記載
-       });   
+    $.ajax({
+      url: 'https://yesno.wtf/api', //アクセスするURL
+      type: 'get',    //post or get
+      cache: false,        //cacheを使うか使わないかを設定
+      dataType: 'json',     //data type script・xmlDocument・jsonなど
+      // data:parameter,           //アクセスするときに必要なデータを記載      
+    })
+      .done(function (response) {
+        console.log(response);
+
+        // 条件分岐
+        console.log(response.answer);
+
+        let result = response.answer
+        console.log(result);
+
+        if (result == 'yes') {
+          
+        } else {
+          
+        }
+
+
+        
+        // 条件分岐
+        if (response == yes) {
+          $('.modal-container').addClass('on');
+        } else {
+          $('.modal-container').addClass('on');
+        }
+
+        $('.close-btn-wrap').on('click', function () {
+          $('.modal-container').removeClass('on');
+        })
+      
+
+
+
+
+
+
+        //通信成功時の処理
+        //成功したとき実行したいスクリプトを記載
+      })
+      .fail(function (xhr) {
+        //通信失敗時の処理
+        //失敗したときに実行したいスクリプトを記載
+      })
+      .always(function (xhr, msg) {
+        //通信完了時の処理
+        //結果に関わらず実行したいスクリプトを記載
+      });
 
 
     // ボタンのカッコ
-    })
+  })
 
 
-//↓大外のカッコ
+  // modal
+  $('.modal-btn').on('click', function () {
+    $('.modal-container').addClass('on');
+  })
+  $('.close-btn-wrap').on('click', function () {
+    $('.modal-container').removeClass('on');
+  })
+
+  //↓大外のカッコ
 })
 
 
 
-// modal
-$('.modal-btn').on('click',function(){
-    $('.modal-container').addClass('on');
-    })
-    $('.close-btn-wrap').on('click',function(){
-      $('.modal-container').removeClass('on');
-    })
