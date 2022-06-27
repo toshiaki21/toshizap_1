64970c0ee8ed386f225bbdb9a9e9bfd65bdbe0bf
Rika <mkmtrkk26@gmail.com>
2022-06-27 18:19:04 +0900

    どんどん追加してます

--- script.js
+++ script.js
@@ -68,6 +68,41 @@ $(function () {
 
 
 // スライダーーーーーーーー
+
+// // import Swiper JS
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import 'swiper/css';
+
+// const swiper = new Swiper(...);
+
+
+// // core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+//   ...
+// });
+
+
+//  // import Swiper bundle with all modules installed
+//  import Swiper from 'swiper/bundle';
+
+//  // import styles bundle
+//  import 'swiper/css/bundle';
+
+//  // init Swiper:
+//  const swiper = new Swiper(...);
+
+
+
 const swiper = new Swiper('.swiper', {
  // Optional parameters
     direction: 'vertical',
