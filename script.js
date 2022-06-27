a170770d9d58a88132ea1cd144524facabdebc6f
Rika <mkmtrkk26@gmail.com>
2022-06-27 14:56:05 +0900

    スライダー進めてます

--- script.js
+++ script.js
@@ -67,4 +67,27 @@ $(function () {
 })
 
 
+// スライダーーーーーーーー
+const swiper = new Swiper('.swiper', {
+ // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
 
